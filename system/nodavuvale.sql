6c5eba17a3ba217df292a7b99aeea160d761d731
Jason Cleeland <jason@cleeland.org>
2024-10-15 17:33:02 +1100

    Update DB Schema

--- system/nodavuvale.sql
+++ system/nodavuvale.sql
@@ -1,28 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Oct 10, 2024 at 11:17 PM
--- Server version: 8.2.0
--- PHP Version: 8.2.13
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `nataleira`
---
-
--- --------------------------------------------------------
-
 
 CREATE TABLE `comment_reactions` (
   `id` int NOT NULL,
@@ -32,13 +9,6 @@ CREATE TABLE `comment_reactions` (
   `reacted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `discussions`
---
-
-DROP TABLE IF EXISTS `discussions`;
 CREATE TABLE `discussions` (
   `id` int NOT NULL,
   `user_id` int NOT NULL,
@@ -50,13 +20,6 @@ CREATE TABLE `discussions` (
   `is_news` tinyint(1) DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `discussion_comments`
---
-
-DROP TABLE IF EXISTS `discussion_comments`;
 CREATE TABLE `discussion_comments` (
   `id` int NOT NULL,
   `discussion_id` int NOT NULL,
@@ -66,13 +29,6 @@ CREATE TABLE `discussion_comments` (
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `discussion_reactions`
---
-
-DROP TABLE IF EXISTS `discussion_reactions`;
 CREATE TABLE `discussion_reactions` (
   `id` int NOT NULL,
   `discussion_id` int DEFAULT NULL,
@@ -81,29 +37,15 @@ CREATE TABLE `discussion_reactions` (
   `reacted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `files`
---
-
-DROP TABLE IF EXISTS `files`;
 CREATE TABLE `files` (
   `id` int NOT NULL,
-  `file_type` enum('photo','document') COLLATE utf8mb3_unicode_ci NOT NULL,
+  `file_type` enum('image','document') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `file_path` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
   `file_format` varchar(10) COLLATE utf8mb3_unicode_ci NOT NULL,
   `file_description` text COLLATE utf8mb3_unicode_ci,
   `upload_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `file_links`
---
-
-DROP TABLE IF EXISTS `file_links`;
 CREATE TABLE `file_links` (
   `id` int NOT NULL,
   `file_id` int NOT NULL,
@@ -111,13 +53,6 @@ CREATE TABLE `file_links` (
   `item_id` int DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `individuals`
---
-
-DROP TABLE IF EXISTS `individuals`;
 CREATE TABLE `individuals` (
   `id` int NOT NULL,
   `nodavuvale_id` char(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -136,52 +71,25 @@ CREATE TABLE `individuals` (
   `photo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Triggers `individuals`
---
-DROP TRIGGER IF EXISTS `before_insert_individuals`;
-DELIMITER $$
-CREATE TRIGGER `before_insert_individuals` BEFORE INSERT ON `individuals` FOR EACH ROW BEGIN
-    IF NEW.nodavuvale_id IS NULL THEN
-        SET NEW.nodavuvale_id = UUID();
-    END IF;
-END
-$$
-DELIMITER ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `items`
---
-
-DROP TABLE IF EXISTS `items`;
 CREATE TABLE `items` (
   `item_id` int NOT NULL,
   `detail_type` varchar(100) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `detail_value` text COLLATE utf8mb3_unicode_ci NOT NULL
+  `detail_value` text COLLATE utf8mb3_unicode_ci NOT NULL,
+  `item_identifier` int DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- --------------------------------------------------------
+CREATE TABLE `item_groups` (
+  `id` int NOT NULL,
+  `item_identifier` int NOT NULL,
+  `item_group_name` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
---
--- Table structure for table `item_links`
---
-
-DROP TABLE IF EXISTS `item_links`;
 CREATE TABLE `item_links` (
   `id` int NOT NULL,
   `individual_id` int NOT NULL,
   `item_id` int NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `relationships`
---
-
-DROP TABLE IF EXISTS `relationships`;
 CREATE TABLE `relationships` (
   `id` int NOT NULL,
   `individual_id_1` int NOT NULL,
@@ -189,13 +97,6 @@ CREATE TABLE `relationships` (
   `relationship_type` enum('child','spouse') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `site_settings`
---
-
-DROP TABLE IF EXISTS `site_settings`;
 CREATE TABLE `site_settings` (
   `id` int NOT NULL,
   `name` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
@@ -205,41 +106,6 @@ CREATE TABLE `site_settings` (
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `temp_individuals`
---
-
-DROP TABLE IF EXISTS `temp_individuals`;
-CREATE TABLE `temp_individuals` (
-  `id` int NOT NULL,
-  `first_names` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `aka_names` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `last_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `birth_prefix` enum('exactly','about','after','before') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `birth_year` year DEFAULT NULL,
-  `birth_month` int DEFAULT NULL,
-  `birth_date` int DEFAULT NULL,
-  `death_prefix` enum('exactly','about','after','before') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `death_year` year DEFAULT NULL,
-  `death_month` int DEFAULT NULL,
-  `death_date` int DEFAULT NULL,
-  `gender` enum('male','female','other') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'other',
-  `photo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `submitted_by` int NOT NULL,
-  `status` enum('pending','approved','rejected') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'pending',
-  `original_individual_id` int DEFAULT NULL,
-  `submitted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int NOT NULL,
   `individuals_id` int DEFAULT NULL,
@@ -255,3 +121,102 @@ CREATE TABLE `users` (
   `registration_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
+
+ALTER TABLE `comment_reactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `comment_id` (`comment_id`),
+  ADD KEY `user_id` (`user_id`);
+
+ALTER TABLE `discussions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+ALTER TABLE `discussion_comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `discussion_id` (`discussion_id`),
+  ADD KEY `user_id` (`user_id`);
+
+ALTER TABLE `discussion_reactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `discussion_id` (`discussion_id`),
+  ADD KEY `user_id` (`user_id`);
+
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `file_links`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `file_id` (`file_id`),
+  ADD KEY `individual_id` (`individual_id`),
+  ADD KEY `item_id` (`item_id`);
+
+ALTER TABLE `individuals`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `fk_item_identifier` (`item_identifier`);
+
+ALTER TABLE `item_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `event_identifier` (`item_identifier`);
+
+ALTER TABLE `item_links`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `relationships`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `individual_id_1` (`individual_id_1`),
+  ADD KEY `individual_id_2` (`individual_id_2`);
+
+ALTER TABLE `site_settings`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+
+ALTER TABLE `comment_reactions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `discussions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `discussion_comments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `discussion_reactions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `files`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `file_links`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `individuals`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `items`
+  MODIFY `item_id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `item_groups`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `item_links`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `relationships`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `site_settings`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `relationships`
+  ADD CONSTRAINT `relationships_ibfk_1` FOREIGN KEY (`individual_id_1`) REFERENCES `individuals` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `relationships_ibfk_2` FOREIGN KEY (`individual_id_2`) REFERENCES `individuals` (`id`) ON DELETE CASCADE;
